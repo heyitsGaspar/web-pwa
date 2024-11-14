20aeb46932d245a8f01035c4a0ff0b948d1cd5ed
heyitsGaspar <israel.chay.32@gmail.com>
2024-11-14 17:23:52 -0600

    fix service worker cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,37 +1,36 @@
 /* eslint-disable no-restricted-globals */
 
-const CACHE_NAME = 'courses-cache-v2';
+const CACHE_NAME = "courses-cache-v2";
 
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/favicon.ico',
-  '/logo192.png',
-  '/logo512.png',
-  'https://pwa-api-production-f5fc.up.railway.app/api/courses',
-  // Agrega cualquier otro recurso que necesites cachear aquí
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/favicon.ico",
+  "/logo192.png",
+  "/logo512.png",
+  "/static", // Agrega cualquier otro recurso que necesites cachear aquí
 ];
 
 // Evento de instalación: almacena en caché los recursos especificados
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      console.log('Caching app shell');
+      console.log("Caching app shell");
       return cache.addAll(urlsToCache);
     })
   );
 });
 
 // Evento de activación: elimina cachés antiguas que ya no son necesarias
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
         // eslint-disable-next-line array-callback-return
         cacheNames.map((cache) => {
           if (cache !== CACHE_NAME) {
-            console.log('Deleting old cache:', cache);
+            console.log("Deleting old cache:", cache);
             return caches.delete(cache);
           }
         })
@@ -40,41 +39,27 @@ self.addEventListener('activate', (event) => {
   );
 });
 
-// Evento de fetch: maneja las solicitudes y excluye URLs no necesarias
-self.addEventListener('fetch', (event) => {
-  const url = new URL(event.request.url);
-
-  // Excluir rutas de Vercel y otras URLs no relevantes
-  if (url.origin === 'https://vercel.live' || url.pathname.startsWith('/.well-known/vercel')) {
-    return;
+self.addEventListener("fetch", (event) => {
+  // Asegúrate de que la solicitud es 'GET', está en la carpeta 'static', y usa 'http' o 'https'
+  const isValidRoute =
+    event.request.url.includes("/static/") ||
+    event.request.url.startsWith("http://") ||
+    event.request.url.startsWith("https://");
+  if (event.request.method === "GET" && isValidRoute) {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        // Si el recurso está en caché, lo devuelve, de lo contrario, realiza la solicitud de red
+        return (
+          response ||
+          fetch(event.request).then((fetchResponse) => {
+            // Clona la respuesta y almacénala en caché
+            return caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, fetchResponse.clone());
+              return fetchResponse;
+            });
+          })
+        );
+      })
+    );
   }
-
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return (
-        response ||
-        fetch(event.request)
-          .then((networkResponse) => {
-            // Solo almacena en caché respuestas JavaScript y HTML exitosas
-            if (
-              networkResponse &&
-              networkResponse.status === 200 &&
-              (networkResponse.headers.get('Content-Type').includes('application/javascript') ||
-               networkResponse.headers.get('Content-Type').includes('text/html'))
-            ) {
-              const responseClone = networkResponse.clone();
-              caches.open(CACHE_NAME).then((cache) => {
-                cache.put(event.request, responseClone);
-              });
-            }
-            return networkResponse;
-          })
-          .catch((error) => {
-            console.error('Error fetching resource:', error);
-            // Devuelve la página de inicio en caso de error
-            return caches.match('/index.html');
-          })
-      );
-    })
-  );
 });
