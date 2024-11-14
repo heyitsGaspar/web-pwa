3821acf9cf579835e9ce33dc6bc572aa673f8245
heyitsGaspar <israel.chay.32@gmail.com>
2024-11-13 23:13:25 -0600

    Cambiando funcionamiento offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -26,7 +26,27 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      return (
+        response ||
+        fetch(event.request)
+          .then((networkResponse) => {
+            if (
+              networkResponse &&
+              networkResponse.status === 200 &&
+              networkResponse.headers.get('Content-Type').includes('application/javascript')
+            ) {
+              const responseClone = networkResponse.clone();
+              caches.open(CACHE_NAME).then((cache) => {
+                cache.put(event.request, responseClone);
+              });
+            }
+            return networkResponse;
+          })
+          .catch((error) => {
+            console.error('Error fetching resource:', error);
+            return caches.match('/index.html');
+          })
+      );
     })
   );
 });
